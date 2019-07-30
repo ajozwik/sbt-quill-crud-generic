f97593ae73b4c6a3563fe95a6974686f9c776dfd
Andrzej Jozwik <andrzej.jozwik@gmail.com>
2019-07-30 20:30:47 +0200

    DateQuotes mandatory

--- src/sbt-test/jdbc/person/src/test/scala/pl/jozwik/example/AbstractQuillSpec.scala
+++ src/sbt-test/jdbc/person/src/test/scala/pl/jozwik/example/AbstractQuillSpec.scala
@@ -2,10 +2,11 @@ package pl.jozwik.example
 
 import io.getquill.{ H2JdbcContext, SnakeCase }
 import org.scalatest.BeforeAndAfterAll
+import pl.jozwik.quillgeneric.quillmacro.quotes.DateQuotes
 import pl.jozwik.quillgeneric.quillmacro.sync.QuillCrudWithContext
 
 trait AbstractQuillSpec extends AbstractSpec with BeforeAndAfterAll {
-  lazy protected val ctx = new H2JdbcContext(SnakeCase, "h2") with QuillCrudWithContext
+  lazy protected val ctx = new H2JdbcContext(SnakeCase, "h2") with QuillCrudWithContext with DateQuotes
 
   override def afterAll(): Unit = {
     ctx.close()
