88c58ff2aeada861c1b958470a9bded015778cfc
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2019-08-15 18:09:43 +0200

    Default settings missing

--- src/main/scala/pl/jozwik/quillgeneric/sbt/PluginKeys.scala
+++ src/main/scala/pl/jozwik/quillgeneric/sbt/PluginKeys.scala
@@ -8,5 +8,6 @@ trait PluginKeys {
   val generateDescription       = settingKey[Seq[RepositoryDescription]]("The repositories descriptions")
   val quillMacroVersion         = settingKey[String]("Quill macro version")
 
-  val defaultSettings: Seq[Def.Setting[_]] = Seq(generateDescription := Seq.empty, quillMacroVersion := "0.7.0-SNAPSHOT")
+  val defaultSettings: Seq[Def.Setting[_]] =
+    Seq(generateDescription := Seq.empty, generateMonixRepositories := Seq.empty, quillMacroVersion := "0.7.0-SNAPSHOT")
 }
