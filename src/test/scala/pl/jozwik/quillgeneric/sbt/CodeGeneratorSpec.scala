4e0117ed5adad45e5f6c427b60cce6eaec9a9c97
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2019-08-15 22:25:23 +0200

    uncomment tests

--- src/test/scala/pl/jozwik/quillgeneric/sbt/CodeGeneratorSpec.scala
+++ src/test/scala/pl/jozwik/quillgeneric/sbt/CodeGeneratorSpec.scala
@@ -40,25 +40,25 @@ abstract class AbstractCodeGeneratorSpec(generator: Generator) extends AbstractS
         content should include(dob)
       }
 
-//      "Generate code for Configuration" in {
-//        val description                   = RepositoryDescription("Configuration", BeanIdClass("ConfigurationId"), "PersonRepository")
-//        val (file: File, content: String) = generateAndLog(description)
-//        file.exists() shouldBe false
-//        content should include(description.beanSimpleClassName)
-//        content should include(description.beanIdSimpleClassName)
-//        content should include(description.repositorySimpleClassName)
-//        content should include(description.toTableName)
-//      }
-//
-//      "Generate code for Sale" in {
-//        val description                   = RepositoryDescription("Sale", BeanIdClass("SaleId", KeyType.Composite), "SaleRepository")
-//        val (file: File, content: String) = generateAndLog(description)
-//        file.exists() shouldBe false
-//        content should include(description.beanSimpleClassName)
-//        content should include(description.beanIdSimpleClassName)
-//        content should include(description.repositorySimpleClassName)
-//        content should include(description.toTableName)
-//      }
+      "Generate code for Configuration" in {
+        val description                   = RepositoryDescription("Configuration", BeanIdClass("ConfigurationId"), "PersonRepository")
+        val (file: File, content: String) = generateAndLog(description)
+        file.exists() shouldBe false
+        content should include(description.beanSimpleClassName)
+        content should include(description.beanIdSimpleClassName)
+        content should include(description.repositorySimpleClassName)
+        content should include(description.toTableName)
+      }
+
+      "Generate code for Sale" in {
+        val description                   = RepositoryDescription("Sale", BeanIdClass("SaleId", KeyType.Composite), "SaleRepository")
+        val (file: File, content: String) = generateAndLog(description)
+        file.exists() shouldBe false
+        content should include(description.beanSimpleClassName)
+        content should include(description.beanIdSimpleClassName)
+        content should include(description.repositorySimpleClassName)
+        content should include(description.toTableName)
+      }
     }
 
   private def generateAndLog(description: RepositoryDescription) = {
