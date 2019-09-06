6d691ae1acb042706dd895f3441bfebae490c813
Andrzej Jozwik <ajozwik@dotsystems.pl>
2019-09-06 09:30:19 +0200

    format code/extract context

--- src/main/scala/pl/jozwik/quillgeneric/sbt/generator/WithTry.scala
+++ src/main/scala/pl/jozwik/quillgeneric/sbt/generator/WithTry.scala
@@ -3,6 +3,6 @@ package pl.jozwik.quillgeneric.sbt.generator
 trait WithTry {
   protected def monad: String       = "Try"
   protected def monadImport: String = s"import util.$monad"
-  protected def tryStart: String    = "      Try {"
-  protected def tryEnd: String      = "      }"
+  protected def tryStart: String    = "Try {"
+  protected def tryEnd: String      = "}"
 }
