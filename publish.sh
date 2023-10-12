76386dc1ddbd095e1019d843b97a1a33171ac41f
ajozwik <andrzej.jozwik@gmail.com>
2023-10-12 23:51:25 +0200

    quill 4.8.0

--- publish.sh
+++ publish.sh
@@ -1,4 +1,2 @@
 git clean -x -d -f src/sbt-test
-. $HOME/.sbt/.sbt-quill-crud-generic-coverage
-sbt8 -Dquill.macro.log=false clean coverage test coverageAggregate && sbt8 coveralls || exit 1
-PATH=$HOME/bin:$PATH exec sbt8 -Dquill.macro.log=false clean test scripted publishSigned sonatypeRelease
+PATH=$HOME/bin:$PATH exec sbt -Dquill.macro.log=false clean test scripted publishSigned sonatypeRelease
